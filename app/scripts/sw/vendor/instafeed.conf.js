4a6bfd33182610ab39d44eabbdaaccefdb4c9cc1
Nathan Reuter <nathan0reuter@gmail.com>
2018-09-21 18:28:59 -0300

    playing with instafeed

--- app/scripts/sw/vendor/instafeed.conf.js
+++ app/scripts/sw/vendor/instafeed.conf.js
@@ -1,10 +1,15 @@
 (function () {
   'use strict';
-
   var feed = new Instafeed({
     get: 'user',
-    userId: '2254432013',
-    clientId: '2f19beb208d447b6a1d2f0c4ca151489'
+    userId: '425048461',
+    clientId: '2f19beb208d447b6a1d2f0c4ca151489',
+    success: function (suc) {
+      console.log('suc', suc);
+    },
+    err: function (err) {
+      console.log('err', err);
+    }
   });
 
   feed.run();
